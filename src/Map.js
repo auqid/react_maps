16d240d557eea80cc07ef9a24bde5bd5308690f2
auqid <auqidirfan100@gmail.com>
2023-12-16 15:15:54 +0530

    border size reduced

--- src/Map.js
+++ src/Map.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { MapContainer, TileLayer, CircleMarker, Popup } from 'react-leaflet';
 
+
+
 const Map = ( {data} ) => {
  console.log(data)
  
@@ -22,20 +24,21 @@ const Map = ( {data} ) => {
         // Access Latitude and Longitude from __parsed_extra array
         const latitude = parseFloat(entry.__parsed_extra[0]);
         const longitude = parseFloat(entry.__parsed_extra[1]);
-        const color = (aqs)=>{
-          if( aqs >=81 && aqs<=100  ){
-            return '#00B0f0'
-          }else if ( aqs >=61 && aqs<=80  ){
-            return '#5ED2A'
-          }else if ( aqs >=41 && aqs<=60  ){
-            return '#edb91e'
-          }else if ( aqs >=21 && aqs<=40  ){
-            return '#EE6B38'
-          }else if ( aqs >=0 && aqs<=20  ){
-            return '#FF2323'
-        }else return 'gray'
-      }
-
+        const getColor = (aqs) => {
+          if (aqs >= 81 && aqs <= 100) {
+            return '#00B0f0';
+          } else if (aqs >= 61 && aqs <= 80) {
+            return '#5ED2A';
+          } else if (aqs >= 41 && aqs <= 60) {
+            return '#edb91e';
+          } else if (aqs >= 21 && aqs <= 40) {
+            return '#EE6B38';
+          } else if (aqs >= 0 && aqs <= 20) {
+            return '#FF2323';
+          } else {
+            return 'gray';
+          }
+        };
         // Check if both Latitude and Longitude are valid numbers
         if (!isNaN(latitude) && !isNaN(longitude)) {
           return (
@@ -44,15 +47,17 @@ const Map = ( {data} ) => {
               position={[latitude, longitude]}
               center={[latitude, longitude]}
               radius={6} // Adjust the radius as needed
-              fillColor={color(entry.AQS)} // Adjust the color as needed
+              fillColor={getColor(entry.AQS)} // Adjust the color as needed
               fillOpacity={1}
+              weight={0.5}
+              
             >
               <Popup>
-                Date: {entry.Date}<br />
+                {/* Date: {entry.Date}<br /> */}
                 VOC, ppm: {entry['VOC, ppm']}<br />
                 AQS: {entry.AQS}<br />
-                Temperature, °C: {entry['Temperature, °C']}<br />
-                Humidity, %: {entry['Humidity, %']}<br />
+                {/* Temperature, °C: {entry['Temperature, °C']}<br /> */}
+                {/* Humidity, %: {entry['Humidity, %']}<br /> */}
                 {/* Add other properties as needed */}
               </Popup>
             </CircleMarker>
